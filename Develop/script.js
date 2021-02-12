33a39cd76a82749f6fffa370aeb5d134ea93f003
Marcia Skousen <marciaskousen@Marcias-iMac.local>
2021-02-11 21:31:32 -0700

    add password user prompts

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,10 @@ function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.textContent = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
     
 //set global variables
     var passRandom = "";
@@ -24,9 +28,27 @@ function writePassword() {
 };
 
     function setPassword() {
-        //
-    }
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+        //user prompts to determine password contents
+        var confNumbers = confirm("Do you want numbers in your password?");
+        if (confNumbers === true) {
+            charAvail = "0123456789";     
+        }
+        var confUpperLetter = confirm("Do you want UPPER case letters in your password?");
+        if (confUpperLetter ===true) {
+            charAvail = charAvail + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        var confLowerLetter = confirm("Do you want lower case letters in your password?");
+        if (confLowerLetter === true) {
+            charAvail = charAvail + "abcdefghijklmnopqrstuvwxyz";
+        }
+        var confSpecial = confirm("Do you want special characters in your password?");
+        if (confSpecial === true) {
+            charAvail = charAvail + "~!@#$%^&*?\|,_-";
+        }
+        // Create loop to generate password, with Charcount determining the number of iterations and charAvail.length 
+        // determining the number of characters to choose from
+        for (var i=0; i< charCount; i++) {
+            var randomChar = Math.floor(Math.random()*charAvail.length);
+            
+        }
   
